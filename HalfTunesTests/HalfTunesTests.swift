0de0ab5760ee91a1388031ce9c44ef355c2adae3
AMIT KISHNANI <akishnani@apple.com>
2017-10-22 02:06:27 -0700

    Initial commit 	new file:   HalfTunes.xcodeproj/project.pbxproj 	new file:   HalfTunes.xcodeproj/xcuserdata/amitkishnani.xcuserdatad/xcschemes/HalfTunes.xcscheme 	new file:   HalfTunes.xcodeproj/xcuserdata/amt1.xcuserdatad/xcschemes/HalfTunes.xcscheme 	new file:   HalfTunes.xcodeproj/xcuserdata/amt1.xcuserdatad/xcschemes/xcschememanagement.plist 	new file:   HalfTunes.xcodeproj/xcuserdata/kentoh.xcuserdatad/xcschemes/HalfTunes.xcscheme 	new file:   HalfTunes.xcodeproj/xcuserdata/kentoh.xcuserdatad/xcschemes/xcschememanagement.plist 	new file:   HalfTunes.xcodeproj/xcuserdata/ronnyglotzbach.xcuserdatad/xcschemes/HalfTunes.xcscheme 	new file:   HalfTunes.xcodeproj/xcuserdata/ronnyglotzbach.xcuserdatad/xcschemes/xcschememanagement.plist 	new file:   HalfTunes/AppDelegate.swift 	new file:   HalfTunes/Base.lproj/LaunchScreen.xib 	new file:   HalfTunes/Base.lproj/Main.storyboard 	new file:   HalfTunes/DHURLSessionMock.swift 	new file:   HalfTunes/Download.swift 	new file:   HalfTunes/Images.xcassets/AppIcon.appiconset/Contents.json 	new file:   HalfTunes/Info.plist 	new file:   HalfTunes/SearchViewController.swift 	new file:   HalfTunes/Track.swift 	new file:   HalfTunes/TrackCell.swift 	new file:   HalfTunesFakeTests/HalfTunesFakeTests.swift 	new file:   HalfTunesFakeTests/Info.plist 	new file:   HalfTunesFakeTests/abbaData.json 	new file:   HalfTunesTests/HalfTunesTests.swift 	new file:   HalfTunesTests/Info.plist

---
+++ HalfTunesTests/HalfTunesTests.swift
@@ -0,0 +1,79 @@
+//
+//  HalfTunesTests.swift
+//  HalfTunesTests
+//
+//  Created by AMIT KISHNANI on 10/21/17.
+//  Copyright © 2017 Ray Wenderlich. All rights reserved.
+//
+
+import XCTest
+
+@testable import HalfTunes
+
+class HalfTunesTests: XCTestCase {
+
+    var sessionUnderTest: URLSession!
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sessionUnderTest = URLSession(configuration: URLSessionConfiguration.default)
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sessionUnderTest = nil
+        super.tearDown()
+    }
+    
+    func testValidCallToiTunesGetsHTTPStatusCode200() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let url = URL(string: "https://itunes.apple.com/search?media=music&entity=song&term=abba")
+        let promise = expectation(description: "Status code: 200")
+
+        
+        let dataTask = sessionUnderTest.dataTask(with: url!) { data, response, error in
+            
+            if let error = error {
+                XCTFail("Error: \(error.localizedDescription)")
+                return
+            } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                if statusCode == 200 {
+                    // 2
+                    promise.fulfill()
+                } else {
+                    XCTFail("Status code: \(statusCode)")
+                }
+            }
+        }
+        dataTask.resume()
+        waitForExpectations(timeout: 5, handler: nil);
+    }
+
+    func testCallToiTunesCompletes() {
+        // given
+        let url = URL(string: "https://itunes.apple.com/search?media=music&entity=song&term=abba")
+        // 1
+        let promise = expectation(description: "Completion handler invoked")
+        var statusCode: Int?
+        var responseError: Error?
+        
+        // when
+        let dataTask = sessionUnderTest.dataTask(with: url!) { data, response, error in
+            statusCode = (response as? HTTPURLResponse)?.statusCode
+            responseError = error
+            // 2
+            promise.fulfill()
+        }
+        dataTask.resume()
+        // 3
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        // then
+        XCTAssertNil(responseError)
+        XCTAssertEqual(statusCode, 200)
+    }
+    
+}
