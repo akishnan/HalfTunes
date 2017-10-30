098071d760ddeb8225243e37f96c6a20bb02665c
AMIT KISHNANI <amitkishnani@comcast.net>
2017-10-30 03:47:00 -0700

    break the build

--- HalfTunesUITests/HalfTunesUITests.swift
+++ HalfTunesUITests/HalfTunesUITests.swift
@@ -37,6 +37,6 @@ class HalfTunesUITests: XCTestCase {
         songNameOrArtistSearchField.typeText("Lata Mangeshkar")
         app.typeText("\r")
         
-        XCTAssertEqual(app.tables.cells.count, 49, "No of songs found not equal to 50")
+        XCTAssertEqual(app.tables.cells.count, 48, "No of songs found not equal to 50")
     }
 }
